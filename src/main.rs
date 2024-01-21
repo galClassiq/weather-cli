8f6fd0fb6b39ab24e8e3e21da620649e8d8e9ebe
galClassiq <gal@classiq.io>
2024-01-21 10:55:17 +0200

    working forecast command

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct WeatherResponse {    
-    temp_c : f32
+struct WeatherResponse {
+    temp_c : f32,
+    forecast: Option<Vec<f32>>
 }
 
 impl WeatherResponse {
@@ -17,32 +18,41 @@ impl WeatherResponse {
         let url = Url::parse(&*url)?;
         println!("{}", url);
         let res = reqwest::get(url).await?.json::<serde_json::Value>().await?;
-        //unwrap current field of res or handle error   
-        match res.get("current") {
-            Some(x) => {
-                match _x.get("temp_c") {  
-                    Some(y) => {
-                        println!("current field found");
-                        let weather_res = WeatherResponse {temp_c: y.as_f64().unwrap() as f32};
-                        Ok(weather_res)
-                    },
-                    None => {
-                        println!("temp_c field not found");
-                        let weather_res = WeatherResponse {temp_c: 0.0};
-                        Ok(weather_res)
-                    }
-
-                }
-            },
-            None => {
-                println!("current field not found");
-                let weather_res = WeatherResponse {temp_c: 0.0};
-                Ok(weather_res)
-            }
-
-        } 
-
         
+        let temp = res.get("current").and_then(|x| x.get("temp_c")).and_then(|x| x.as_f64());
+        match temp {
+            Some(x) => Ok(WeatherResponse{temp_c: x as f32, forecast: None}),
+            None => Err(ExitFailure::from(std::io::Error::new(std::io::ErrorKind::Other, "API Error")))
+        }
+    }
+    async fn get_today(location: &String, api_key: &String) -> Result<Self, ExitFailure> {
+        let url = format!(
+            "http://api.weatherapi.com/v1/forecast.json?key={}&q={}&days=1&aqi=no&alerts=no",
+            location, api_key);
+
+        let url = Url::parse(&*url)?;
+        // println!("{}", url);
+        let res = reqwest::get(url).await?.json::<serde_json::Value>().await?;
+        let forcastday = res.get("forecast").and_then(|x| x.get("forecastday"));
+        let hours = forcastday.and_then(|x| x[0].get("hour")).unwrap().to_owned();
+        if let Some(hours_array) = hours.as_array(){
+            let mut temps = vec![];
+            for hour in hours_array {
+                let temp = hour.get("temp_c").and_then(|x| x.as_f64());
+                match temp {
+                    Some(x) => temps.push(x as f32),
+                    None => continue
+                }
+            }
+            return Ok(WeatherResponse {
+                temp_c: 0.0,
+                forecast: Some(temps)})
+        }
+       
+        Ok(WeatherResponse {
+            temp_c: 0.0,
+            forecast: None})
+
     }
 }
 
@@ -57,8 +67,9 @@ async fn main() -> Result<(), ExitFailure>{
         location = args[1].clone();
     }
     println!("{}",location);
-    let res = WeatherResponse::get(&api_key,&location).await?;
-    println!("{}'s current temperature: {:?}", location, res);
-    println!("{}", location);
+    // let res = WeatherResponse::get(&api_key,&location).await?;
+    // println!("{}'s current temperature: {:?}", location, res);
+    let forecast = WeatherResponse::get_today(&api_key,&location).await?;
+    println!("{}'s forecast: {:?}", location, forecast);
     Ok(())
 }
