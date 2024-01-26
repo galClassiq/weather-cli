f191c3388e6ac770c852f76079248d47b165a8ef
Gal Winer <gal@classiq.io>
2024-01-26 15:11:20 +0200

    working forecast
    working 2 command cli
    
    two working commands

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
-use std::env;
+
 use exitfailure::ExitFailure;
 use reqwest::Url;
 use serde_derive::{Deserialize, Serialize};
 use clap::Parser;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct WeatherResponse {    
-    temp_c : f32
+enum WeatherResponse {
+    Current {temp_c : f32},
+    Forecast {temp_vec : Vec<(String,f32)>},
 }
-
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Get the curent weather for a location
     #[arg(short, long)]
-    fetch_current: String,
+    current: Option<String>,
 
+    #[arg(short, long)]
+    forecast : Option<String>,
+
+
+}
+
+#[derive(Deserialize, Debug)]
+struct ForecastHour{
+    temp_c : f32,
+    time : String
+}
+#[derive(Deserialize, Debug)]
+struct ForecastDay{
+    hour : Vec<ForecastHour>
+}
+#[derive(Deserialize, Debug)]
+struct ForecastDays{
+    forecastday : Vec<ForecastDay>
+}
+#[derive(Deserialize, Debug)]
+struct WeatherForecast {
+    forecast: ForecastDays
 }
 
 impl WeatherResponse {
@@ -30,7 +51,7 @@ impl WeatherResponse {
             Some(x) => {
                 match x.get("temp_c") {  
                     Some(y) => {
-                        let weather_res = WeatherResponse {temp_c: y.as_f64().unwrap() as f32};
+                        let weather_res = WeatherResponse::Current {temp_c: y.as_f64().unwrap() as f32};
                         Ok(weather_res)
                     },
                     None => {
@@ -48,7 +69,18 @@ impl WeatherResponse {
 
         
     }
+
+    async fn fetch_forecast(location: &String, api_key: &String) -> Result<Self, ExitFailure> {
+        const BASE_URL: &str = "http://api.weatherapi.com/v1/forecast.json";
+        let url = Url::parse_with_params(BASE_URL, &[("key", api_key), ("q", location)]).unwrap(); 
+        let res = reqwest::get(url.as_str()).await?.json::<WeatherForecast>().await?;
+        let mut tempVec = vec![];
+        let temp = res.forecast.forecastday[0].hour.iter().for_each(|x| tempVec.push((x.time.to_owned(),x.temp_c)));
+        Ok(WeatherResponse::Forecast {temp_vec: tempVec})
+        
     
+    }
+
 }
 
 
@@ -58,9 +90,16 @@ const API_KEY:&'static str = "fa8e4f9240a04e5fa32202525241601";
 async fn main() -> Result<(), ExitFailure>{
     
     let args = Args::parse();
-    let res = WeatherResponse::fetch_current_weather(&args.fetch_current,&API_KEY.to_owned()).await?;
-    println!("{}'s current temperature: {:}c", &args.fetch_current, res.temp_c);
-    Ok(())
+    if let Some(current) = &args.current {
+        let res = WeatherResponse::fetch_current_weather(current,&API_KEY.to_owned()).await?;
+        println!("{}'s current temperature: {:?}c", current, res);
+    }
+    if let Some(forecast) = &args.forecast {
+        let res = WeatherResponse::fetch_forecast(forecast,&API_KEY.to_owned()).await?;
+        println!("{}'s current temperature: {:?}c", forecast, res);
+    }   
+
+    Ok(())    
 }
 
 #[tokio::test]
@@ -68,3 +107,4 @@ async fn test_get_current() {
     let res = WeatherResponse::fetch_current_weather(&String::from("nyc"),&API_KEY.to_owned()).await;
     assert!(res.is_ok());
 }
+
